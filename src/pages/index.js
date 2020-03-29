dd1775a4f36121b7b7bb08e436c6d2de5a26fa2c
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-03-29 19:32:39 +0200

    Code clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,14 @@
-import { graphql, Link } from "gatsby"
-import Img from 'gatsby-image'
 import React from "react"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from '../components/seo'
 
 
-export const data = graphql`
-  {
-    wpgraphql {
-      postBy(slug: "triggertanker-hva-er-de-og-hvordan-kan-de-hjelpe-deg") {
-        slug
-        featuredImage {
-          imageFile {
-            childImageSharp {
-              fluid {
-                ...GatsbyImageSharpFluid_withWebp_tracedSVG
-              }
-            }
-          }
-          sourceUrl
-        }
-      }
-    }
-  }
-`;
-
-
-const IndexPage = ({ data }) => {
+const IndexPage = () => {
   return (
     < Layout >
       <SEO title="Home" />
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
-      <p>{data.wpgraphql.postBy.slug}</p>
-      <Img fluid={data.wpgraphql.postBy.featuredImage.imageFile.childImageSharp.fluid} />
-      <Link to="/page-2/">Go to page 2</Link>
     </Layout >
   )
 }
