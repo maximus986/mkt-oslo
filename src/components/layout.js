c27ac60f7a714f6c51ad344df0250828cec941aa
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-03-30 21:27:12 +0200

    Change component export type

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,13 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from "react"
+import React from 'react'
 import { Header } from '../components/common/header/header'
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+import styled from '@emotion/styled'
 
-import "./layout.css"
 
-const Layout = ({ children }) => {
+import './layout.css'
+
+export const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -26,13 +21,16 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <Main>{children}</Main>
     </>
   )
 }
+
+const Main = styled.main`
+  margin-top: 228px;
+`;
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
