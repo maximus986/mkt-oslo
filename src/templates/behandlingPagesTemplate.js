e69a2317c0441e86154cbf4c7bd8204766ac6e6d
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-04-01 20:49:36 +0200

    Implement logic for showing sidebar

--- src/templates/behandlingPagesTemplate.js
+++ src/templates/behandlingPagesTemplate.js
@@ -5,6 +5,7 @@ import { Layout } from '../components/layout'
 import { Container } from '../components/common/container'
 import { Grid } from 'theme-ui'
 import SEO from '../components/seo'
+import styled from '@emotion/styled'
 
 const BehandlingPagesTemplate = props => {
   const { data: {
@@ -12,8 +13,10 @@ const BehandlingPagesTemplate = props => {
   }
   } = props
 
-  const { content, pageTitle } = page
-
+  const { content, pageTitle, uri } = page
+  const isRootBehandling = uri === 'behandling/'
+  console.log(isRootBehandling)
+  console.log(uri)
   return (
     <Layout>
       <SEO title={pageTitle} />
@@ -21,7 +24,7 @@ const BehandlingPagesTemplate = props => {
         <Grid
           gap={7}
           columns={[1, 1, '1fr 2fr', '1fr 2fr', '3fr 9fr']}>
-          <aside sx={{ bg: 'yellow' }}>Sidebar</aside>
+          <Aside sx={{ bg: 'yellow' }} show={isRootBehandling}>Sidebar</Aside>
           <div sx={{ bg: '#ccc' }} dangerouslySetInnerHTML={{ __html: content }} />
         </Grid>
       </Container>
@@ -31,6 +34,11 @@ const BehandlingPagesTemplate = props => {
 
 export default BehandlingPagesTemplate;
 
+const Aside = styled.aside`
+  @media(max-width: 768px) {
+    display: ${props => props.show ? 'block' : 'none'};
+  }
+`;
 
 export const pageQuery = graphql`
   query GET_BEHANDLING_PAGE($id: ID!) {
