d85be33e8d9928544cb8411a5d3e2bea712e7983
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-04-02 12:49:53 +0200

    Remove container component

--- src/components/common/sectionContainer.js
+++ src/components/common/sectionContainer.js
@@ -2,22 +2,20 @@
 import { jsx } from 'theme-ui'
 import React from 'react';
 import styled from '@emotion/styled'
-import { Container } from './container'
+import { HeadingLine } from './headingLine'
 
 export const SectionContainer = ({ title, children }) => {
   return (
     <section sx={{
-      pt: ['70px', null, '100px']
+      py: ['70px', null, '100px']
     }}>
-      <Container>
-        <div sx={{ textAlign: 'center', mb: 7 }}>
-          <SectionTitle sx={{
-            mb: 4
-          }}>{title}</SectionTitle>
-          <SectionLine></SectionLine>
-        </div>
-        {children}
-      </Container>
+      <div sx={{ textAlign: 'center', mb: 7 }}>
+        <SectionTitle sx={{
+          mb: 4
+        }}>{title}</SectionTitle>
+        {title && <HeadingLine />}
+      </div>
+      {children}
     </section>
   );
 }
@@ -26,22 +24,5 @@ const SectionTitle = styled.h2`
   text-transform: uppercase;
 `
 
-const SectionLine = styled.span`
-  display: inline-block;
-  width: 65px;
-  height: 1px;
-  margin: 0 auto;
-  background: #ccc;
-  position: relative;
-  top: -6px;
-  margin-bottom: 4px;
-  &:after {
-    content: "";
-    width: 40px;
-    height: 1px;
-    display: block;
-    background: #ccc;
-    margin: 3px auto;
-  }
-`
+
 
