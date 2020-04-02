26463ad30b1eb182abe8df59fadde52e556858c1
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-04-02 22:12:02 +0200

    Add section subtitle

--- src/components/common/sectionContainer.js
+++ src/components/common/sectionContainer.js
@@ -4,16 +4,23 @@ import React from 'react';
 import styled from '@emotion/styled'
 import { HeadingLine } from './headingLine'
 
-export const SectionContainer = ({ title, children }) => {
+export const SectionContainer = ({ title, subtitle, children }) => {
   return (
     <section sx={{
       py: ['70px', null, '100px']
     }}>
       <div sx={{ textAlign: 'center', mb: 7 }}>
-        <SectionTitle sx={{
-          mb: 4
-        }}>{title}</SectionTitle>
+        <h2 sx={{
+          mb: 4,
+          textTransform: 'uppercase'
+        }}>{title}</h2>
         {title && <HeadingLine />}
+        <h5 sx={{
+          fontSize: '14px',
+          color: 'grey400',
+          letterSpacing: '2px',
+          textTransform: 'uppercase'
+        }}>{subtitle}</h5>
       </div>
       {children}
     </section>
