b2be4b8e6ec1c0c5f6fae1d183fb81bd45ec5188
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-04-02 12:49:34 +0200

    Abstract heading line to a separate component

---
+++ src/components/common/headingLine.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+
+export const HeadingLine = styled.span`
+  display: inline-block;
+  width: 65px;
+  height: 1px;
+  margin: 0 auto;
+  background: #ccc;
+  position: relative;
+  top: -6px;
+  margin-bottom: 4px;
+  &:after {
+    content: "";
+    width: 40px;
+    height: 1px;
+    display: block;
+    background: #ccc;
+    margin: 3px auto;
+  }
+`
