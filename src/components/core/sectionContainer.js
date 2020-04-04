9b489cbc03eb155e1af7a5ac5448f2251966aa64
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-04-04 19:58:35 +0200

    Remove space if  there is no subtitle

--- src/components/core/sectionContainer.js
+++ src/components/core/sectionContainer.js
@@ -10,17 +10,22 @@ export const SectionContainer = ({ title, subtitle, children }) => {
       py: ['70px', null, '100px']
     }}>
       <div sx={{ textAlign: 'center', mb: 7 }}>
-        <h2 sx={{
-          mb: 4,
-          textTransform: 'uppercase'
-        }}>{title}</h2>
-        {title && <HeadingLine />}
-        <h5 sx={{
-          fontSize: '14px',
-          color: 'grey400',
-          letterSpacing: '2px',
-          textTransform: 'uppercase'
-        }}>{subtitle}</h5>
+        <div sx={{ mb: 16 }}>
+          <h2 sx={{
+            mb: 4,
+            textTransform: 'uppercase'
+          }}>{title}</h2>
+          {title && <HeadingLine />}
+          {
+            subtitle && <h5 sx={{
+              fontSize: '14px',
+              color: 'grey400',
+              letterSpacing: '2px',
+              textTransform: 'uppercase',
+              m: 0
+            }}>{subtitle}</h5>
+          }
+        </div>
       </div>
       {children}
     </section>
