bf25b398a1a41c96c8265e623a3c8da8abd47346
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-04-05 01:19:52 +0200

    Clean gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,6 @@
-import React from 'react';
-import { LoadScriptNext } from '@react-google-maps/api';
-
 require("typeface-lato")
 require("typeface-montserrat")
 require("typeface-nothing-you-could-do")
 
-export const wrapRootElement = ({ element }) => (
-  <LoadScriptNext
-    googleMapsApiKey={process.env.GATSBY_GOOGLE_MAP_API_KEY}
-    preventGoogleFontsLoading={false}
-  >
-    {element}
-  </LoadScriptNext>
-)
+
 
