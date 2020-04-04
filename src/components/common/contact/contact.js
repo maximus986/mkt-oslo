6e1b9e55127cb225e225ef3f1ec3f3f0b08fd031
Aleksandar Maksimovic <aleksandar.maksimovic@p3ds.net>
2020-04-05 01:13:27 +0200

    Add test map

--- src/components/common/contact/contact.js
+++ src/components/common/contact/contact.js
@@ -2,7 +2,7 @@
 import { jsx, Grid } from 'theme-ui'
 import { ContactInfo } from './contactInfo'
 import { SectionContainer, Container } from '../../core/index'
-import { Map } from '../contact/map'
+import Map from '../contact/testMap'
 import { Form } from '../contact/form'
 
 export const Contact = () => {
@@ -13,6 +13,7 @@ export const Contact = () => {
           <ContactInfo />
           <Grid gap={[11, null, 0]} columns={[1, 1, '1fr 1fr']}>
             <Form />
+            {/* <Map /> */}
             <Map />
           </Grid>
         </Container>
--- src/components/common/contact/map.js
+++ src/components/common/contact/map.js
@@ -10,53 +10,55 @@ import Image from 'gatsby-image'
 //   width: "800px"
 // }
 
-const position = {
-  lat: 59.942429,
-  lng: 10.703862
-}
+// const position = {
+//   lat: 59.942429,
+//   lng: 10.703862
+// }
 
-const center = {
-  lat: 59.942515,
-  lng: 10.703862,
-}
-
-
-
-export const Map = () => {
-  //   const { staticMap } = useStaticQuery(graphql`
-  //   {
-  //     staticMap {
-  //       mapUrl
-  //       childFile {
-  //         childImageSharp {
-  //           fluid {
-  //             ...GatsbyImageSharpFluid_withWebp_tracedSVG
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
-  //   const { mapUrl, childFile: { childImageSharp: { fluid } } } = staticMap
-  //   return (
-  //     <a href={mapUrl}>
-  //       <Image fluid={fluid} alt="Map" />
-  //     </a>
-  //   )
-  // const { isLoaded, loadError } = useLoadScript({
-  //   googleMapsApiKey: `${process.env.GOOGLE_MAP_API_KEY}`,
-  // }, [])
-
-  return <GoogleMap
-    mapContainerStyle={{ width: '100%', height: '610px' }}
-    zoom={15}
-    center={center}
-    options={{ styles: mapStyles }}
-  >
-    <Marker
-      position={position}
-    />
-  </GoogleMap >
-}
+// const center = {
+//   lat: 59.942515,
+//   lng: 10.703862,
+// }
 
 
+
+// export const Map = () => {
+//   const { staticMap } = useStaticQuery(graphql`
+//   {
+//     staticMap {
+//       mapUrl
+//       childFile {
+//         childImageSharp {
+//           fluid {
+//             ...GatsbyImageSharpFluid_withWebp_tracedSVG
+//           }
+//         }
+//       }
+//     }
+//   }
+// `)
+//   const { mapUrl, childFile: { childImageSharp: { fluid } } } = staticMap
+//   return (
+//     <a href={mapUrl}>
+//       <Image fluid={fluid} alt="Map" />
+//     </a>
+//   )
+// const { isLoaded, loadError } = useLoadScript({
+//   googleMapsApiKey: `${process.env.GOOGLE_MAP_API_KEY}`,
+// }, [])
+
+
+// return <GoogleMap
+//   mapContainerStyle={{ width: '100%', height: '610px' }}
+//   zoom={15}
+//   center={center}
+//   options={{ styles: mapStyles }}
+// >
+//   <Marker
+//     position={position}
+//   />
+// </GoogleMap >
+
+// }
+
+export const Map = () => (<div>sdfsdf</div>)
